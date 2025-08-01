e7c753876ce2e2c467072c27f5f57d230396733e
Khant Thit Oo <khantthitoo.dev@gmail.com>
2025-08-01 11:21:51 +0630

    Docker Implemented

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+import dj_database_url
 
 import environ
 
@@ -104,10 +105,7 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR("db.sqlite3"),
-    }
+    "default": dj_database_url.config(default=env("DATABASE_URL"))
 }
 
 
