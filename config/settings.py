a5a006bcf47702ddd3e9444a23c6956fb23be912
Khant Thit Oo <khantthitoo.dev@gmail.com>
2025-08-01 15:59:57 +0630

    Switched Database

--- config/settings.py
+++ config/settings.py
@@ -105,7 +105,10 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default=env("DATABASE_URL"))
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR("db.sqlite3"),
+    }
 }
 
 
